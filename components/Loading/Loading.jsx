dfd1d87aefec542bedb53c121d206ee80132bef7
Jason <jasonlikenfs@gmail.com>
2017-12-20 15:33:33 +0800

    :wrench: missing spin css

--- components/Loading/Loading.jsx
+++ components/Loading/Loading.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import 'spinkit/css/spinners/10-fading-circle.css';
+import 'spinkit/css/spinners/10-fading-circle.css';
 
 class Loading extends React.Component {
   constructor(props) {
